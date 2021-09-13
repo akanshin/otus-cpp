48e13f093f30e8d154004e456ba3d9091187ff18
Artemiy Kanshin <djkanshin@gmail.com>
2021-09-13 20:58:25 +0700

    Added version.

--- src/otusapp/main.cpp
+++ src/otusapp/main.cpp
@@ -2,6 +2,8 @@
 
 int main(int argc, char** argv)
 {
+    std::cout << "otusapp version " << PROJECT_VERSION << std::endl;
+    
     otus::Greeting greeting;
     std::cout << greeting.sayHello() << std::endl;
     return EXIT_SUCCESS;
--- src/otusapp/otusapp_precompiled.h
+++ src/otusapp/otusapp_precompiled.h
@@ -1,6 +1,8 @@
 #ifndef OTUS_CPP_OTUSAPP_PRECOMPILED_H
 #define OTUS_CPP_OTUSAPP_PRECOMPILED_H
 
+#include <version.h>
+
 #include <cstdlib>
 #include <iostream>
 #include <string>
